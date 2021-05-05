ed0f789d2cf5d0d2692c7dc8d81439d2b19d72d6
Ricardas B <no@email.com>
2021-05-05 22:37:16 +0200

    Add URL configuration of API endpoints

---
+++ src/ro_api_app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from . import views
+
+
+
+urlpatterns = [
+    path('authors/', views.AuthorList.as_view(), name='list-of-authors'),
+    path('books/', views.BookList.as_view(), name='list-of-books'),
+    path('quotes/', views.QuoteList.as_view(), name='list-of-quotes'),
+    path('tags/', views.TagList.as_view(), name='list-of-tags'),
+    path('authors/<int:pk>/', views.AuthorDetails.as_view(), name='author-details'),
+    path('books/<int:pk>/', views.BookDetails.as_view(), name='book-details'),
+    path('quotes/<int:pk>/', views.QuoteDetails.as_view(), name='quote-details'),
+    path('tags/<int:pk>/', views.TagDetails.as_view(), name='tag-details'),
+
+]
