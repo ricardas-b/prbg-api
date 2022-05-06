258726ecbdfc4b3799fca883ed4817d9d05d2382
Ricardas B <ricardas.brazinskas@gmail.com>
2022-05-07 01:18:38 +0200

    Adjust formatting

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,7 +1,9 @@
 from dotenv import dotenv_values
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
