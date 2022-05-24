5eca5a9826bbb909860d58f71b983f94e702a195
Ricardas B <ricardas.brazinskas@gmail.com>
2022-05-24 23:36:12 +0200

    Remove print statements

--- src/quote_admin_app/views.py
+++ src/quote_admin_app/views.py
@@ -26,7 +26,6 @@ def show_quote_with_tags(request, pk):
                 tag.append(True)
             else:
                 tag.append(False)
-        #print('\nall_tags', all_tags, '\n')
 
         context = {
             'pk': pk,
@@ -49,29 +48,18 @@ def update_tags(request, pk):
     if request.method == 'POST':
 
         old_tag_selection = [tag['tag_id'] for tag in QuoteTag.objects.filter(quote=pk).values()]
-        print('selected_tags', old_tag_selection)
-
         new_tag_selection = list(request.POST.getlist('tag_selection'))
         new_tag_selection = [int(i) for i in new_tag_selection]
-        print('new_tag_selection', new_tag_selection)
 
         for tag_id in new_tag_selection:
             if tag_id not in old_tag_selection:
                 quote = Quote.objects.get(pk=pk)
-                #print('quote', quote)
-                #print('quote id', pk)
                 tag = Tag.objects.get(pk=tag_id)
-                #print('tag', tag)
-                #print('tag_id', tag_id)
                 QuoteTag(quote=quote, tag=tag).save()
 
         for tag_id in old_tag_selection:
             if tag_id not in new_tag_selection:
-                print('quote', pk)
-                print('tag', tag_id)
                 qs = QuoteTag.objects.get(quote=pk, tag=tag_id)
-                print(qs)
-                print(type(qs))
                 qs.delete()
 
         return redirect(f'/api/v1/show-quote/{pk}/')
